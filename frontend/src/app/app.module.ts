5c57f58cee5706b9bdbd2809d6d3b70e24d9fbfa
Lagger <franek616@gmail.com>
2018-06-13 22:30:10 +0200

    bajerek refaktor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,25 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
 import { OneDayScheduleComponent } from './schedule/one-day-schedule/one-day-schedule.component';
 import { NavbarComponent } from './navbar/navbar/navbar.component';
-import { ScheduleService } from './schedule.service';
+import { ScheduleService } from './services/schedule.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FakeScheduleService } from './services/fake-schedule.service';
+import { RouterModule, Routes } from '@angular/router';
+import { ScheduleTableComponent} from './schedule/schedule-table/schedule-table.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/schedule',
+    pathMatch: 'full'
+  },
+  { path: 'schedule',
+    component: ScheduleTableComponent,
+  },
+];
 
 
 @NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    HttpClientModule,
+  ],
   declarations: [
     AppComponent,
     OneDayScheduleComponent,
     NavbarComponent,
+    ScheduleTableComponent,
   ],
-  imports: [
-    BrowserModule,
-    RouterModule,
-    HttpModule
+  providers: [
+    { provide: ScheduleService, useClass: FakeScheduleService },
   ],
-  providers: [ScheduleService],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+
